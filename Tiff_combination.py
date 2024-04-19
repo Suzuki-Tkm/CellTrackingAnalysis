8c79e6010791054a471ce73f4bef39dee0f2a7ce
TakumiSuzuki <takumi.s.1230@icloud.com>
2024-04-19 16:12:49 +0900

    tiff file combination

---
+++ Tiff_combination.py
@@ -0,0 +1,23 @@
+from PIL import Image
+import os
+
+def combine_to_multi_page_tiff(folder_path, output_file):
+    tiff_files = sorted([file for file in os.listdir(folder_path) if file.endswith('.tiff') or file.endswith('.tif')])
+    if not tiff_files:
+        print("No TIFF files found in the folder.")
+        return
+
+    images = []
+    
+    for file_name in tiff_files:
+        image = Image.open(os.path.join(folder_path, file_name))
+        images.append(image)
+    
+    images[0].save(output_file, save_all=True, append_images=images[1:])
+    print(f"Combined {len(tiff_files)} TIFF files into {output_file}")
+
+# tiffフォルダの指定
+folder_path = "/Users/apple/研究/data/小田切先生研究データ/閉じにくい"
+# tiffのファイルの指定
+output_file = "/Users/apple/研究/data/小田切先生研究データ/output.tiff"
+combine_to_multi_page_tiff(folder_path, output_file)
