f21b6c243b2c41b718a4742b7798a9bd0598925c
TakumiSuzuki <takumi.s.1230@icloud.com>
2024-04-19 11:27:25 +0900

    PythonでimageJ操作（テンプレート）

---
+++ Python_to_ImageJ.py
@@ -0,0 +1,15 @@
+# ライブラリのインポート
+import imagej
+
+# ゲートウェイを作成する
+ij = imagej.init()
+
+# 画像を読み込む
+image_url = 'https://imagej.net/images/FluorescentCells.jpg'
+jimage = ij.io().open(image_url)
+
+# 配列に変換
+image = ij.py.from_java(jimage)
+
+# 画像を表示する
+ij.py.show(image)
